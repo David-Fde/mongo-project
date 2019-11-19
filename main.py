77350e9467ee11ac6dac91bea5d4c848ec44b837
David-Fde <fernandezvazquezd@gmail.com>
2019-11-19 19:36:24 +0100

    mongodb actualizado con bases de datos

--- main.py
+++ main.py
@@ -5,13 +5,9 @@ from dotenv import load_dotenv
 load_dotenv()
 import requests,json,folium
 import src.maps_api as ma
-apikey = os.getenv("KEY")
-#df = ma.get_coords_from_api()   
 
 df = apikey.search_places_by_coordinate("40.819057,-73.914048", "100", "restaurant")
 
-#df.to_csv("./output/dataset.csv", index=False)
-print(df)
 def main():
     if __name__=='__main__':
         main()
