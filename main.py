20ad999edce39ac82b7346bf01cb1cc52415c11c
David-Fde <fernandezvazquezd@gmail.com>
2019-11-19 17:01:09 +0100

    Pagination: done

--- main.py
+++ main.py
@@ -5,10 +5,13 @@ from dotenv import load_dotenv
 load_dotenv()
 import requests,json,folium
 import src.maps_api as ma
+apikey = os.getenv("KEY")
+#df = ma.get_coords_from_api()   
 
-df = ma.get_coords_from_api()   
-df.to_csv("./output/dataset.csv", index=False)
+df = apikey.search_places_by_coordinate("40.819057,-73.914048", "100", "restaurant")
 
+#df.to_csv("./output/dataset.csv", index=False)
+print(df)
 def main():
     if __name__=='__main__':
         main()
