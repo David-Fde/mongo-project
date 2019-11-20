63aee8d03c2289c5d74445ef487c0d68cf924a8a
David-Fde <fernandezvazquezd@gmail.com>
2019-11-20 10:40:36 +0100

    Creando pipeline para subir geoindexes

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from pymongo import MongoClient
 from datetime import datetime
 import os
+import pandas as pd
 from dotenv import load_dotenv
 load_dotenv()
 import requests,json,folium
 import src.functions as fnc
-import pandas as pd
 
-places = pd.DataFrame(fnc.search_places_by_input())
-places.to_json("./output/dataset.json", orient="records")
+'''
+    Download data of a place from places.api and save them to a .json file.
+
+    #places = pd.DataFrame(fnc.search_places_by_input())
+    #places.to_json("./output/dataset.json", orient="records")
+'''
 
 
 def main():
--- src/functions.py
+++ src/functions.py
@@ -4,6 +4,7 @@ import time
 import os
 from dotenv import load_dotenv
 load_dotenv()
+from pymongo import MongoClient
 
 def search_places_by_input(query=''):
     apiKey = os.getenv("KEY")
@@ -26,3 +27,31 @@ def search_places_by_input(query=''):
     return places
 
 
+def connectCollection(database, collection):
+    client = MongoClient()
+    db = client[database]
+    coll = db[collection]
+    return db, coll
+    
+
+def getLocation(places_coordinates):
+    location=[]
+    for i in range(len(places_coordinates)):        
+        longitude = places_coordinates[i]['geometry']['location']['lng']
+        latitude = places_coordinates[i]['geometry']['location']['lat']
+        loc = {
+            'type':'Point',
+            'coordinates':[longitude,latitude]
+        }
+        location.append(loc)
+    return location
+
+def create_geoindex(location):
+    for place in location:
+        value = {"$set": {'location':getLocation(place)}}
+        coll.update_one(place,value)
+
+
+db, coll = connectCollection("companies",input("Collection name: "))
+
+places_coordinates = list(coll.find({"geometry.location.lat":{"$ne":None},"geometry.location.lng":{"$ne":None}}))
